59e4583c8801c8ccf77cc47f6bb98bdffd5f207c
jabdalqader <jabdalqader@Madfooat.com>
2020-10-30 00:01:09 +0300

    Adding firebase remote config

--- app/src/main/java/com/madfooatcom/test/Test/MainActivity.kt
+++ app/src/main/java/com/madfooatcom/test/Test/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.madfooatcom.test.Test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.Signature
+import android.os.Build
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.madfooatcom.efawateercomsdktest.BuildConfig
 import com.madfooatcom.efawateercomsdktest.common.entrance.Efawateercom
-
+import com.madfooatcom.test.BuildConfig.APPLICATION_ID
 import com.madfooatcom.test.R
 import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import kotlin.experimental.and
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +30,29 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+//        try {
+//            Log.wtf("ASSSSSSS", "qwr")
+//            if (Build.VERSION.SDK_INT >= 28) {
+//                @SuppressLint("WrongConstant") val packageInfo = packageManager.getPackageInfo(
+//                    com.madfooatcom.test.BuildConfig.APPLICATION_ID,
+//                    PackageManager.GET_SIGNING_CERTIFICATES
+//                )
+//                val signatures: Array<Signature> = packageInfo.signingInfo.apkContentsSigners
+//                val md = MessageDigest.getInstance("SHA")
+//                Log.wtf("ASSSSSSS0", "asd")
+//                for (signature in signatures) {
+//                    md.update(signature.toByteArray())
+//                    val signatureBase64 = String(Base64.encode(md.digest(), Base64.DEFAULT))
+//                    Log.wtf("ASSSSSSS1", md.digest().toString())
+//                }
+//            }
+//        } catch (e: PackageManager.NameNotFoundException) {
+//            e.printStackTrace()
+//            e.message?.let { Log.wtf("ASSSSSSS2", it) }
+//        } catch (e: NoSuchAlgorithmException) {
+//            e.printStackTrace()
+//            e.message?.let { Log.wtf("ASSSSSSS3", it) }
+//        }
 //        val remoteConfig = Firebase.remoteConfig
 //        val configSettings = remoteConfigSettings {
 //            minimumFetchIntervalInSeconds = 1
@@ -36,4 +72,6 @@ class MainActivity : AppCompatActivity() {
 //
 //        TestRemoteConfig.runRemoteConfig(this);
     }
+
+
 }
--- efawateercomsdktest/src/main/java/com/madfooatcom/efawateercomsdktest/TestActivity.kt
+++ efawateercomsdktest/src/main/java/com/madfooatcom/efawateercomsdktest/TestActivity.kt
@@ -1,17 +1,23 @@
 package com.madfooatcom.efawateercomsdktest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.Signature
+import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
-import com.madfooatcom.efawateercomsdktest.common.di.modules.makeRemoteConfigDataModule
+import android.util.Base64
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.madfooatcom.efawateercomsdktest.data.entities.RemoteConfigData
-import com.madfooatcom.efawateercomsdktest.domain.usecases.GetRemoteConfigUseCase
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_test.*
 import org.koin.android.ext.android.inject
-import org.koin.core.context.loadKoinModules
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import kotlin.experimental.and
+
 
 internal class TestActivity : AppCompatActivity() {
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
@@ -23,8 +29,91 @@ internal class TestActivity : AppCompatActivity() {
         setContentView(R.layout.activity_test)
 
         textView.text = remoteConfigData.configuration?.app?.environment?.type
+        //getKeyHash("SHA")
 
+        get(this,"SHA")
+//        try {
+//            Log.wtf("ASSSSSSS", "qwr")
+//            if (Build.VERSION.SDK_INT >= 28) {
+//                @SuppressLint("WrongConstant") val packageInfo = packageManager.getPackageInfo(
+//                    BuildConfig.LIBRARY_PACKAGE_NAME, PackageManager.GET_SIGNING_CERTIFICATES
+//                )
+//                val signatures: Array<Signature> = packageInfo.signingInfo.apkContentsSigners
+//                val md = MessageDigest.getInstance("SHA")
+//                Log.wtf("ASSSSSSS0", "asd")
+//                for (signature in signatures) {
+//                    md.update(signature.toByteArray())
+//                    val signatureBase64 = String(Base64.encode(md.digest(), Base64.DEFAULT))
+//                    Log.wtf("ASSSSSSS1", signatureBase64)
+//                }
+//            }
+//        } catch (e: PackageManager.NameNotFoundException) {
+//            e.printStackTrace()
+//            e.message?.let { Log.wtf("ASSSSSSS2", it) }
+//        } catch (e: NoSuchAlgorithmException) {
+//            e.printStackTrace()
+//            e.message?.let { Log.wtf("ASSSSSSS3", it) }
+//        }
     }
+
+
+    @SuppressLint("PackageManagerGetSignatures")
+    operator // test purpose
+    fun get(context: Context, key: String) {
+        try {
+            val info: PackageInfo = context.getPackageManager()
+                .getPackageInfo(
+                    BuildConfig.LIBRARY_PACKAGE_NAME,
+                    PackageManager.GET_SIGNATURES
+                )
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance(key)
+                md.update(signature.toByteArray())
+                val digest = md.digest()
+                val toRet = StringBuilder()
+                for (i in digest.indices) {
+                    if (i != 0) toRet.append(":")
+                    val b: Byte = digest[i] and 0xff.toByte()
+                    val hex = Integer.toHexString(b.toInt())
+                    if (hex.length == 1) toRet.append("0")
+                    toRet.append(hex)
+                }
+                Log.wtf("ASSSSSSS0", String(toRet))
+            }
+        } catch (e1: PackageManager.NameNotFoundException) {
+            Log.e("name not found", e1.toString())
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("no such an algorithm", e.toString())
+        } catch (e: Exception) {
+            Log.e("exception", e.toString())
+        }
+    }
+
+//    private fun getKeyHash(hashStretagy: String) {
+//        val info: PackageInfo
+//        try {
+//            Log.e("Dasfasf", "Dasfasf")
+//            info = packageManager.getPackageInfo(
+//                BuildConfig.LIBRARY_PACKAGE_NAME,
+//                PackageManager.GET_SIGNATURES
+//            )
+//            for (signature in info.signatures) {
+//                var md: MessageDigest
+//                md = MessageDigest.getInstance(hashStretagy)
+//                md.update(signature.toByteArray())
+//                val something = String(Base64.encode(md.digest(), 0))
+//                Log.e("KeyHash  -->>>>>>>>>>>>", something)
+//                textView.text = something
+//                // Notification.registerGCM(this);
+//            }
+//        } catch (e1: PackageManager.NameNotFoundException) {
+//            Log.e("name not found", e1.toString())
+//        } catch (e: NoSuchAlgorithmException) {
+//            Log.e("no such an algorithm", e.toString())
+//        } catch (e: java.lang.Exception) {
+//            Log.e("exception21435", e.toString())
+//        }
+//    }
 
     override fun onDestroy() {
         super.onDestroy()
